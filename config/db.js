7d58c94668fab4681de15df8f117f11cde652e48
Evan Washington <enavy04@gmail.com>
2024-03-03 23:54:20 -0800

    Migrated db setup to config folder.

---
+++ config/db.js
@@ -0,0 +1,28 @@
+import mongoose from 'mongoose';
+import pusher from './pusher.js';
+const connection_url = process.env.MONGODB_WHATSAPP_URI;
+
+
+// connect to mongoDB Atlas, and create a "listener" that detects when insertions to the database
+
+mongoose.connect(connection_url);
+const db = mongoose.connection;
+db.once('open', () => {
+    console.log('Database connected');
+
+    const msgCollection = db.collection('messagecontents');
+    const changeStream = msgCollection.watch();
+    changeStream.on('change', (change) => {
+        if(change.operationType === 'insert') {
+            const messageDetails = change.fullDocument;
+            pusher.trigger('messages', 'inserted', {
+                name: messageDetails.name,
+                message: messageDetails.message
+            });
+        } else {
+            console.log('Error triggering Pusher');
+        }
+    })
+});
+
+export default mongoose;
--- server.js
+++ server.js
@@ -2,32 +2,10 @@ import express from "express";
 import mongoose from "mongoose";
 import Messages from './dbMessages.js';
 import bodyParser from 'body-parser';
-import pusher from './config/pusher.js';
+import db from './config/db.js';
 
 const app = express();
 const port = process.env.PORT || 9000;
-
-// connect to mongoDB Atlas, and create a "listener" that detects when insertions to the database
-const connection_url = process.env.MONGODB_WHATSAPP_URI;
-mongoose.connect(connection_url);
-const db = mongoose.connection;
-db.once('open', () => {
-    console.log('Database connected');
-
-    const msgCollection = db.collection('messagecontents');
-    const changeStream = msgCollection.watch();
-    changeStream.on('change', (change) => {
-        if(change.operationType === 'insert') {
-            const messageDetails = change.fullDocument;
-            pusher.trigger('messages', 'inserted', {
-                name: messageDetails.name,
-                message: messageDetails.message
-            });
-        } else {
-            console.log('Error triggering Pusher');
-        }
-    })
-})
 
 // CORS Headers
 app.use((req, res, next) => {
