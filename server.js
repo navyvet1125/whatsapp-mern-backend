3e9b8d235cba16ed1ceb11848ce87716371755bc
Evan Washington <enavy04@gmail.com>
2024-03-03 23:14:23 -0800

    Add Pusher functionality.  Added listener for inserts to the database.

--- server.js
+++ server.js
@@ -2,12 +2,45 @@ import express from "express";
 import mongoose from "mongoose";
 import Messages from './dbMessages.js';
 import bodyParser from 'body-parser';
+import Pusher from 'pusher';
 
 const app = express();
 const port = process.env.PORT || 9000;
+
+// Creates new Pusher object that tells Pusher API what account and app to use
+const pusher = new Pusher({
+  appId: process.env.PUSHER_WHATSAPP_MERN_APPID,
+  key: process.env.PUSHER_WHATSAPP_MERN_KEY,
+  secret: process.env.PUSHER_WHATSAPP_MERN_SECRET,
+  cluster: process.env.PUSHER_WHATSAPP_MERN_CLUSTER,
+  useTLS: true
+});
+
+// pusher.trigger("my-channel", "my-event", {
+//   message: "hello world"
+// });
+
 const connection_url = process.env.MONGODB_WHATSAPP_URI;
 
 mongoose.connect(connection_url);
+const db = mongoose.connection;
+db.once('open', () => {
+    console.log('Database connected');
+
+    const msgCollection = db.collection('messagecontents');
+    const changeStream = msgCollection.watch();
+    changeStream.on('change', (change) => {
+        if(change.operationType === 'insert') {
+            const messageDetails = change.fullDocument;
+            pusher.trigger('messages', 'inserted', {
+                name: messageDetails.user,
+                message: messageDetails.message
+            });
+        } else {
+            console.log('Error triggering Pusher');
+        }
+    })
+})
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
