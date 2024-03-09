72d75a4084b35e440a752c6b8c7ddc317e73b846
Evan Washington <enavy04@gmail.com>
2024-03-09 12:16:03 -0800

    Removed request model

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -1,13 +1,11 @@
 import User from '../models/user.js';
-import Request from '../models/request.js';
 const controller = {};
 
 controller.read = async (req, res) => {
     try{
         const username = req.params.username
         const user =  await User.findOne({username}).select('email name username id joined');
-        const friends = await Request.findFriendsByID(user.id);
-        res.status(200).send({user, friends});
+        res.status(200).send({user});
 
     } catch (err) {
         console.log(err);
--- seed.js
+++ seed.js
@@ -1,13 +1,11 @@
 import mongoose from 'mongoose';
 import User from './models/user.js';
-import Request from './models/request.js';
 
 const connection_url = process.env.MONGODB_WHATSAPP_URI;
 
 mongoose.connect(connection_url);
 
 await User.deleteMany({name: {$exists: true}});
-await Request.deleteMany({_id: {$exists: true}})
 console.log('Database connected: ');
 try {
     const users = await User.create([
@@ -16,15 +14,7 @@ try {
         { name:'Dwight Washington', email:'daw54@aol.com', username:'daw54', password:'whodadaddy313!'}
     ]);
 
-    // console.log(users[0].id);
-    // await User.addRequest(users[0].id, users[1].id);
     await users[0].addRequest(users[1]);
-    // const requests = await  Request.create([
-    //     {members: [users[0], users[1]]},
-    //     {members: [users[0], users[2]], request: 'accepted'},
-    //     {members: [users[2], users[1]]}
-    // ])
-    // console.log(requests);
     console.log('Database seeded...');
 } catch (err) {
     console.log(err);
