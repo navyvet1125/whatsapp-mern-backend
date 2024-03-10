f7139299c3445d0ad5c9d52d59b7c55d47f6794a
Evan Washington <enavy04@gmail.com>
2024-03-10 15:14:17 -0700

    Updated message model.

--- models/message.js
+++ models/message.js
@@ -1,14 +1,12 @@
 import mongoose from "mongoose";
 import User from "./user.js";
+import Chat from "./chat.js";
 
 const whatsAppSchema = mongoose.Schema({
     message: String,
-    sender: {type: mongoose.Schema.Types.ObjectId, ref: 'User'},
-    receiver: {type: mongoose.Schema.Types.ObjectId, ref: 'User'},
-    name: String,
+    chat: {type: mongoose.Schema.Types.ObjectId, ref: 'Chat'},
     timeStamp: {type: Date, default: Date.now()},
-    received: {type: Boolean, default: false},
-    isTyping: {type: Boolean, default: false}
+    received: {type: Boolean, default: false}
 });
 
 export default mongoose.model('messageContent',whatsAppSchema);
